7e3be8aac822c5581a89971f37b40628061bb5d2
Ömer <109592399+omdgn@users.noreply.github.com>
2025-09-23 21:41:48 +0300

    Update university.controller.js

--- not-kutusu-backend/src/controllers/university.controller.js
+++ not-kutusu-backend/src/controllers/university.controller.js
@@ -1,6 +1,8 @@
 const University = require("../models/university.model");
 const User = require("../models/user.model");
 const Note = require("../models/note.model");
+const Comment = require("../models/comment.model");
+const Course = require("../models/course.model");
 
 // Üniversiteleri listele (herkes erişebilir)
 const getAllUniversities = async (req, res) => {
@@ -15,16 +17,84 @@ const getAllUniversities = async (req, res) => {
 // Global istatistikler (herkes erişebilir)
 const getGlobalStats = async (req, res) => {
   try {
-    const [universityCount, userCount, noteCount] = await Promise.all([
+    const [
+      universityCount,
+      userCount,
+      noteCount,
+      commentCount,
+      totalLikes,
+      topUniversities,
+      topCourses
+    ] = await Promise.all([
       University.countDocuments(),
       User.countDocuments(),
-      Note.countDocuments()
+      Note.countDocuments(),
+      Comment.countDocuments(),
+      Note.aggregate([
+        { $group: { _id: null, total: { $sum: "$likes" } } }
+      ]).then(result => result[0]?.total || 0),
+      Note.aggregate([
+        { $group: { _id: "$universityId", noteCount: { $sum: 1 } } },
+        { $sort: { noteCount: -1 } },
+        { $limit: 10 },
+        {
+          $lookup: {
+            from: "University",
+            localField: "_id",
+            foreignField: "_id",
+            as: "university"
+          }
+        },
+        { $unwind: "$university" },
+        {
+          $project: {
+            name: "$university.name",
+            slug: "$university.slug",
+            noteCount: 1
+          }
+        }
+      ]),
+      Note.aggregate([
+        { $group: { _id: "$courseId", noteCount: { $sum: 1 } } },
+        { $sort: { noteCount: -1 } },
+        { $limit: 10 },
+        {
+          $lookup: {
+            from: "Course",
+            localField: "_id",
+            foreignField: "_id",
+            as: "course"
+          }
+        },
+        { $unwind: "$course" },
+        {
+          $lookup: {
+            from: "University",
+            localField: "course.universityId",
+            foreignField: "_id",
+            as: "university"
+          }
+        },
+        { $unwind: "$university" },
+        {
+          $project: {
+            courseName: "$course.name",
+            courseCode: "$course.code",
+            universityName: "$university.name",
+            noteCount: 1
+          }
+        }
+      ])
     ]);
 
     res.status(200).json({
       universityCount,
       userCount,
-      noteCount
+      noteCount,
+      commentCount,
+      totalLikes,
+      topUniversities,
+      topCourses
     });
   } catch (err) {
     res.status(500).json({ message: "Sunucu hatası", error: err.message });
