12579b9ace65a64f5de3039c60f495e94010d35a
Ömer <109592399+omdgn@users.noreply.github.com>
2025-09-23 21:29:49 +0300

    Update university.controller.js

--- not-kutusu-backend/src/controllers/university.controller.js
+++ not-kutusu-backend/src/controllers/university.controller.js
@@ -1,4 +1,6 @@
 const University = require("../models/university.model");
+const User = require("../models/user.model");
+const Note = require("../models/note.model");
 
 // Üniversiteleri listele (herkes erişebilir)
 const getAllUniversities = async (req, res) => {
@@ -10,6 +12,55 @@ const getAllUniversities = async (req, res) => {
   }
 };
 
+// Global istatistikler (herkes erişebilir)
+const getGlobalStats = async (req, res) => {
+  try {
+    const [universityCount, userCount, noteCount] = await Promise.all([
+      University.countDocuments(),
+      User.countDocuments(),
+      Note.countDocuments()
+    ]);
+
+    res.status(200).json({
+      universityCount,
+      userCount,
+      noteCount
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Sunucu hatası", error: err.message });
+  }
+};
+
+// Üniversiteye özgü istatistikler (slug bazlı)
+const getUniversityStats = async (req, res) => {
+  try {
+    const { slug } = req.params;
+
+    const university = await University.findOne({ slug });
+    if (!university) {
+      return res.status(404).json({ message: "Üniversite bulunamadı" });
+    }
+
+    const [userCount, noteCount] = await Promise.all([
+      User.countDocuments({ universityId: university._id }),
+      Note.countDocuments({ universityId: university._id })
+    ]);
+
+    res.status(200).json({
+      university: {
+        name: university.name,
+        slug: university.slug
+      },
+      userCount,
+      noteCount
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Sunucu hatası", error: err.message });
+  }
+};
+
 module.exports = {
-  getAllUniversities
+  getAllUniversities,
+  getGlobalStats,
+  getUniversityStats
 };
